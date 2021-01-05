4bf3a3e3215abd73b00dcbc291cbe41870681ee5
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-01-05 12:47:39 +1100

    Add groups to outout

--- frontend/ci/push.sh
+++ frontend/ci/push.sh
@@ -2,7 +2,19 @@
 
 set -o errexit -o nounset -o pipefail
 
+cd $(dirname "$0")/..
+
+. ../ci/functions.sh
+
+start_group "Installing dependencies"
+npm ci
+end_group
+
+start_group "Building"
 npm run clean
 npm run build
+end_group
 
+start_group "Uploading to s3://$FRONTEND_BUCKET"
 aws s3 sync dist/ "s3://${FRONTEND_BUCKET}/"
+end_group
