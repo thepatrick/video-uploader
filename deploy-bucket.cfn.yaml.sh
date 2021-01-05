3e930a6be3b864d1465567e8076a633e484693df
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-01-05 11:45:53 +1100

    Remove debug output

--- deploy-bucket.cfn.yaml.sh
+++ deploy-bucket.cfn.yaml.sh
@@ -4,10 +4,6 @@ set -o errexit -o nounset -o pipefail
 
 export AWS_DEFAULT_REGION=ap-southeast-2
 
-aws --version
-
 aws cloudformation deploy \
   --template-file backend-bucket.cfn.yaml \
   --stack-name uploader-backend
-
-echo $?
