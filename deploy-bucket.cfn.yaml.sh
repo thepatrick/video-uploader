a9c48dfae0e3de3c8a3a1541b0cbca2a0040847f
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-01-05 11:56:18 +1100

    Output bucket name

--- deploy-bucket.cfn.yaml.sh
+++ deploy-bucket.cfn.yaml.sh
@@ -7,3 +7,7 @@ export AWS_DEFAULT_REGION=ap-southeast-2
 aws cloudformation deploy \
   --template-file backend-bucket.cfn.yaml \
   --stack-name uploader-backend
+
+LAMBDA_BUCKET=$(aws cloudformation describe-stacks --stack-name uploader-backend --query 'Stacks[0].Outputs[?OutputKey==`BackendBucket`].OutputValue' --output text)
+
+echo "::set-output name=backendBucket::$LAMBDA_BUCKET"
