e91cf99b59b49f2bc952da1fa376a7fc42655226
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-01-05 11:22:27 +1100

    Auto deploy backend-bucket.cfn.yaml

---
+++ deploy-bucket.cfn.yaml.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+
+export AWS_PROFILE=lca2021
+export AWS_DEFAULT_REGION=ap-southeast-2
+
+aws cloudformation deploy \
+  --template-file backend-bucket.cfn.yaml \
+  --stack-name uploader-backend
+
