07254c9d86e669bc44eb1b37c57c6ca838ea67c5
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-01-05 11:54:09 +1100

    Auto build backend-lambda

--- backend-lambda/push.sh
+++ backend-lambda/push.sh
@@ -2,7 +2,6 @@
 
 set -o errexit -o nounset -o pipefail
 
-export AWS_PROFILE=lca2021
 export AWS_DEFAULT_REGION=ap-southeast-2
 
 export LAMBDA_BUCKET=$(aws cloudformation describe-stacks --stack-name uploader-backend --query 'Stacks[0].Outputs[?OutputKey==`BackendBucket`].OutputValue' --output text)
