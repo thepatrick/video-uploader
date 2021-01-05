00f59d65c79d9af39f73793d89a2f4b4a0ab7d5b
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-01-05 12:26:24 +1100

    Auto deploy (sort of) deployment.cfn.yaml (2)

--- deploy-deployment.cfn.yaml.sh
+++ deploy-deployment.cfn.yaml.sh
@@ -4,10 +4,7 @@ set -o errexit -o nounset -o pipefail
 
 export AWS_DEFAULT_REGION=ap-southeast-2
 
-BackendLambdaS3Bucket=$(aws cloudformation describe-stacks --stack-name uploader-backend --query 'Stacks[0].Outputs[?OutputKey==`BackendBucket`].OutputValue' --output text)
-FrontendWebsiteCertificate=$(env AWS_DEFAULT_REGION=us-east-1 aws cloudformation describe-stacks --stack-name uploader-certificate --query 'Stacks[0].Outputs[?OutputKey==`Certificate`].OutputValue' --output text)
-BackendLambdaS3Key=$(backend-lambda/ci/current-object.sh) 
-
 echo FrontendWebsiteCertificate=$FrontendWebsiteCertificate
 echo BackendLambdaS3Bucket=$BackendLambdaS3Bucket
 echo BackendLambdaS3Key=$BackendLambdaS3Key
+
