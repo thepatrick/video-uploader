a4fc95ac57383b8e47dcb6043b1ec1a16d188dff
Patrick Quinn-Graham <make-contact@pftqg.com>
2021-01-05 12:35:02 +1100

    Auto deploy (sort of) deployment.cfn.yaml (3)

--- deploy-certificate.cfn.yaml.sh
+++ deploy-certificate.cfn.yaml.sh
@@ -8,9 +8,9 @@ aws cloudformation deploy \
   --template-file certificate.cfn.yaml \
   --stack-name uploader-certificate \
   --parameter-override \
-    HostedZoneId=Z09102521YXWQT2ZKMPP7 \
-    FrontendDomain=lca2021-presenter.aws.nextdayvideo.com.au \
-    BackendDomain=lca2021-presenter-api.aws.nextdayvideo.com.au
+    "HostedZoneId=$HOSTED_ZONE_ID" \
+    "FrontendDomain=$FRONTEND_DOMAIN" \
+    "BackendDomain=$BACKEND_DOMAIN" \
 
 certificate=$(aws cloudformation describe-stacks --stack-name uploader-certificate --query 'Stacks[0].Outputs[?OutputKey==`Certificate`].OutputValue' --output text)
 
--- deploy-deployment.cfn.yaml.sh
+++ deploy-deployment.cfn.yaml.sh
@@ -4,7 +4,22 @@ set -o errexit -o nounset -o pipefail
 
 export AWS_DEFAULT_REGION=ap-southeast-2
 
-echo FrontendWebsiteCertificate=$FrontendWebsiteCertificate
-echo BackendLambdaS3Bucket=$BackendLambdaS3Bucket
-echo BackendLambdaS3Key=$BackendLambdaS3Key
+echo FRONTEND_WEBSITE_CERTIFICATE=$FRONTEND_WEBSITE_CERTIFICATE
+echo BACKEND_LAMBDA_S3_BUCKET=$BACKEND_LAMBDA_S3_BUCKET
+echo BACKEND_LAMBDA_S3_KEY=$BACKEND_LAMBDA_S3_KEY
 
+aws cloudformation deploy \
+  --template-file deployment.cfn.yaml \
+  --stack-name uploader \
+  --no-execute-changeset \
+  --parameter-override \
+    "HostedZoneId=$HOSTED_ZONE_ID" \
+    "FrontendDomain=$FRONTEND_DOMAIN" \
+    "BackendDomain=$BACKEND_DOMAIN" \
+    "FrontendWebsiteCertificate=$FRONTEND_WEBSITE_CERTIFICATE" \
+    "BackendJWTAudience=$BACKEND_JWT_AUDIENCE" \
+    "BackendLambdaS3Bucket=$BACKEND_LAMBDA_S3_BUCKET" \
+    "BackendLambdaS3Key=$BACKEND_LAMBDA_S3_KEY" \
+    "BackendJWTPrivateKey=$BACKEND_JWT_PRIVATE_KEY" \
+    "VeypearPresenterBase=$VEYPEAR_PRESENTER_BASE" \
+  --capabilities CAPABILITY_IAM
